104cd3d9ae358da9963c574bc5ec1df97ef91959
Sarthak Agrawal <sarthak.agrawal@healthplix.com>
2022-06-17 22:19:02 +0530

    Install firebase and connect it to the server by config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +11,16 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+//  Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQlxvwCpQ_sEdnoDO0YsP90tc30rthmDA",
+  authDomain: "todo-list-235e4.firebaseapp.com",
+  projectId: "todo-list-235e4",
+  storageBucket: "todo-list-235e4.appspot.com",
+  messagingSenderId: "776406699357",
+  appId: "1:776406699357:web:c54797144053eb79ce6cb4"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
